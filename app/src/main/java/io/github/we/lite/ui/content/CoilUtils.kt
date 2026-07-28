e9baa996fa4e729dff68bfc6e0bb34b50ab6a3d3
SymonChu <chushusheng@gmail.com>
2026-07-28 13:05:50 +0800

    Delete stale Telegram/panel files that referenced deleted features

--- app/src/main/java/io/github/we/lite/ui/content/CoilUtils.kt
+++ app/src/main/java/io/github/we/lite/ui/content/CoilUtils.kt
@@ -4,7 +4,6 @@ import coil3.ImageLoader
 import coil3.gif.AnimatedImageDecoder
 import coil3.network.okhttp.OkHttpNetworkFetcherFactory
 import coil3.request.CachePolicy
-import io.github.we.lite.features.items.chat.panel.service.FunBoxCrypto
 import io.github.we.lite.utils.HostInfo
 import io.github.we.lite.utils.WeLogger
 import okhttp3.MediaType
@@ -23,7 +22,6 @@ private val imageHttpClient by lazy {
             val originalContentType = response.body.contentType()
             val encrypted = response.body.bytes()
             val decoded = try {
-                FunBoxCrypto.decryptObject(encrypted, securityKey)
             } catch (error: Throwable) {
                 WeLogger.w(TAG, "failed to decode secured image response", error)
                 throw IOException("failed to decode secured image response", error)
