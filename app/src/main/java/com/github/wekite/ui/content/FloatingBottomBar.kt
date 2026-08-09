a4101fe67ec14c5a8a88a32324d1877d8098f85f
SymonChu <chushusheng@gmail.com>
2026-08-09 22:24:08 +0800

    Fix missing fillMaxWidth import in FloatingBottomBar

--- app/src/main/java/com/github/wekite/ui/content/FloatingBottomBar.kt
+++ app/src/main/java/com/github/wekite/ui/content/FloatingBottomBar.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
